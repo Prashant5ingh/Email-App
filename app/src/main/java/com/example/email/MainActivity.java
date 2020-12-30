61c588276526adcf5a7791ac630b29646a3d6750
Prashant5ingh <primeprashant38881@gmail.com>
2020-12-30 21:46:15 +0530

    Text Change of Snackbar

--- app/src/main/java/com/example/email/MainActivity.java
+++ app/src/main/java/com/example/email/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace With Your Own Action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
